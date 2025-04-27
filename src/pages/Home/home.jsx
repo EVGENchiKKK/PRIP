aabf86a669dfd3f9a793b0a3623f2565f70291db
EVGENchiKKK <parshutin05@inbox.ru>
2025-04-27 16:12:05 +0700

    Вёрстка главной страницы ч.4

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,8 @@
 import { SideBarSection } from "../../navigation/headerAside/headerAside";
 import { SearchBar } from "../../components/searchBar/SearchBar";
 import { CallToAction } from "../../components/cards/CallToAction";
+import { FourYouCard } from "../../components/cards/FourYouCard/FourYouCard";
+import { NewRelizCard } from "../../components/cards/NewRelizCard/NewRelizCard";
 import './../../App.css';
 
 export const Home = () => {
@@ -9,6 +11,9 @@ export const Home = () => {
             <SideBarSection />
             <main className="mainPlayPage">
                 <SearchBar/>
+                <h1>Главная</h1>
+                <NewRelizCard />
+                <FourYouCard/>
                 <CallToAction/>
             </main>
         </div>
