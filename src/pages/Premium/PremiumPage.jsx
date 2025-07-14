2018715a6ab5344e6897b3b7cabc656abaa76b0f
EVGENchiKKK <parshutin05@inbox.ru>
2025-07-15 02:42:15 +0700

    Доработка вывода карточчек премиума

--- src/pages/Premium/PremiumPage.jsx
+++ src/pages/Premium/PremiumPage.jsx
@@ -1,12 +1,17 @@
 import { Header } from "../../navigation/header";
 import { Footer } from "../../components/Footer";
-import { ZagViz } from "../../components/cards/HeroSection";
+import { ZagVizPrem } from "../../components/cards/HeroSectionPrem";
+import { PremiumContainer } from "../../components/cards/PremiumContainer/PremiumContainer";
 
 export const PremiumPage = () => {
     return (
         <div className="vizitPage">
             <Header/>
-            <ZagViz/>
+            <ZagVizPrem/>
+            <h2 className="premText">
+                Пользуйся Neofy без ограничений с премиумоми насладись музыкой по полной
+            </h2>
+            <PremiumContainer collectionPlan="premium"/>
             {/* <ZagViz />
             <AboutCard />
             <AdvantagesSection />
